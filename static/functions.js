2e1cb55fc575859cba823165b0f38f8d8f4f7516
PossiblyAHuman <112639806+PossiblyAHuman@users.noreply.github.com>
2024-05-18 00:53:01 -0400

    Add files via upload

--- static/functions.js
+++ static/functions.js
@@ -1,12 +1,43 @@
 function homepage()
 {
-    location.href = "/templates/homepage.html"
+  location.href = "/templates/homepage.html"
 }
 function resume()
 {
-    location.href = "/templates/resume.html"
+  location.href = "/templates/resume.html"
 }
 function interests()
 {
-    location.href = "/templates/interests.html"
+  location.href = "/templates/interests.html"
+}
+function projects()
+{
+  location.href = "/templates/projects.html"
+}
+
+function linkedin()
+{
+  window.open("https://www.linkedin.com/in/azhou7/", "_blank");
+}
+
+function interest_tech() {
+  var box = document.getElementById("techbox");
+
+  if (box.hidden == true)
+  {
+    box.hidden = false
+  } else {
+    box.hidden = true
+  }
+}
+
+function interest_programming() {
+  var box = document.getElementById("programmingbox");
+
+  if (box.hidden == true)
+  {
+    box.hidden = false
+  } else {
+    box.hidden = true
+  }
 }
