0c06fe59da0ed14f51e3b2cee80f483f0c500521
PossiblyAHuman <112639806+PossiblyAHuman@users.noreply.github.com>
2024-05-21 17:11:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
+def homepage1():
     return render_template("homepage.html")
 
-@app.route("/interests")
+@app.route("/templates/homepage.html")
+def homepage2():
+    return render_template("homepage.html")
+
+@app.route("/templates/interests.html")
 def interests():
     return render_template("interests.html")
 
-@app.route("/resume")
+@app.route("/templates/resume.html")
 def resume():
     return render_template("resume.html")
 
+@app.route("/templates/projects.html")
+def project():
+    return render_template("projects.html")
+
+@app.route("/allProjects")
+def allProjects():
+    proj1 = {}
+    proj1["title"] = "proj1"
+    proj1["url"] = "https://www.google.com"
+    proj1["image"] = "../static/me.png"
+
+    proj2 = {}
+    proj2["title"] = "proj2"
+    proj2["url"] = "https://www.facebook.com"
+    proj2["image"] = "../static/githublogo.jpg"
+
+    proj3 = {}
+    proj3["title"] = "proj3"
+    proj3["url"] = "https://www.twitter.com"
+    proj3["image"] = "../static/leetcodelogo.png"
+
+    proj4 = {}
+    proj4["title"] = "proj4"
+    proj4["url"] = "https://www.youtube.com"
+    proj4["image"] = "../static/linkedinlogo.png"
+
+    proj5 = {}
+    proj5["title"] = "proj5"
+    proj5["url"] = "https://www.microsoft.com"
+    proj5["image"] = "../static/githublogo.jpg"
+
+    listie = [proj1, proj2, proj3, proj4, proj5]
+
+    print("listielistie", listie)
+    return listie
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
--- static/functions.js
+++ static/functions.js
@@ -19,6 +19,10 @@ function linkedin()
 {
   window.open("https://www.linkedin.com/in/azhou7/", "_blank");
 }
+function github()
+{
+  window.open("https://github.com/PossiblyAHuman", "_blank");
+}
 
 function interest_tech() {
   var box = document.getElementById("techbox");
@@ -40,4 +44,75 @@ function interest_programming() {
   } else {
     box.hidden = true
   }
+}
+
+
+
+function allProjects() {
+  request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+
+      var response = JSON.parse(this.response)
+
+      length = response.length
+
+      box4 = document.getElementById("Box4");
+      box4.innerHTML = ""
+
+      for (let i = 0; i < length; ) {
+
+        projrow = document.createElement("div");
+        projrow.className = "projectRow";
+
+        let j = i;
+
+        var counter = 0
+        for (; i < length && i < j+3; i++) {
+
+          const projcardcontainer = document.createElement("div");
+          projcardcontainer.className = "projectCardContainer";
+
+          const projcard = document.createElement("div");
+          projcard.className = "projectCard";
+
+          /*projcard.innerHTML = response[i]["title"];*/
+          projcard.style.backgroundImage = "url("+ response[i]["image"] +")";
+          projcard.style.backgroundColor = "white";
+          
+          const projcardtitle = document.createElement("div");
+          projcardtitle.className = "projectCardTitle";
+          projcardtitle.innerHTML = response[i]["title"];
+
+          projcardcontainer.appendChild(projcard);
+          projcardcontainer.appendChild(projcardtitle);
+
+          projrow.appendChild(projcardcontainer);
+          
+          const currentIndex = i;
+          projcardcontainer.onclick = function () {
+            window.open(response[currentIndex]["url"], "_blank");
+          };
+
+          counter++
+        }
+
+        while (counter != 3) {
+          const fillerProjectCard = document.createElement("div");
+          fillerProjectCard.className = "fillerProjectCard";
+          fillerProjectCard.style.backgroundColor = "transparent";
+          projrow.appendChild(fillerProjectCard);
+          counter++
+        }
+
+
+
+        box4.append(projrow)
+      }
+      
+    }
+  }
+
+  request.open("GET", "/allProjects")
+  request.send()
 }
